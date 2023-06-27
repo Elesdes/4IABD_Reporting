6ca83a3b78fe956f3002ed1227b98c998f0d85a5
ErwanDuprey <erwan.duprey@free.fr>
2023-06-27 22:51:31 +0200

    Change to analyze

--- Reporting_&_Restitution.py
+++ Reporting_&_Restitution.py
@@ -1,26 +1,10 @@
 import pandas as pd
 import streamlit as st
 from src.df_utils import generate_df_from_metadata
-from src.reporting import clean_dataset, convert_data, render
+
 
 # Set page configuration
 st.set_page_config(page_title="Welcome !")
 
 st.title("Hello !")
 st.sidebar.header("Discover Our Project.")
-
-data = pd.read_csv("data/Marijuana_Arrests.csv")
-columns = data.columns
-
-for column in columns:
-    dataset, data_type = convert_data(
-        data[column]
-    )  # Convert data to numerical, categorical, text or index
-
-    dataset = dataset.dropna()  # Remove NaN values
-
-    sparsity = 1.0 - len(dataset) / float(
-        len(data[column])
-    )  # 1 - Size after cleaning / Size before cleaning
-
-    render(dataset, column, sparsity, data_type)
--- pages/1_📊_Analyze.py
+++ pages/1_📊_Analyze.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import streamlit as st
+from src.reporting import convert_data, render
 
 # Set page configuration
 st.set_page_config(page_title="Marijuana Arrest In Colombia - Analyze", page_icon="chart_with_upwards_trend")
@@ -12,3 +13,20 @@ st.write(
          Here is our analyze.
          """
 )
+
+
+data = pd.read_csv("data/Marijuana_Arrests.csv")
+columns = data.columns
+
+for column in columns:
+    dataset, data_type = convert_data(
+        data[column]
+    )  # Convert data to numerical, categorical, text or index
+
+    dataset = dataset.dropna()  # Remove NaN values
+
+    sparsity = 1.0 - len(dataset) / float(
+        len(data[column])
+    )  # 1 - Size after cleaning / Size before cleaning
+
+    render(dataset, column, sparsity, data_type)
